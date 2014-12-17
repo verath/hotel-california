e64d42d1042e6cdc8c0ef79f932673ba9cb16a6c
Peter Eliasson <warackta@gmail.com>
2014-12-17 11:24:43 +0100

    Some minor changes to integration-tests project structure.
    * Removed included .jars, instead use eclipse included JUnit.
    * Added main project as a project dependency for the integration-tests project.

---
+++ integration-tests/test/tda593/hotel/california/integration/CheckInIT.java
@@ -0,0 +1,13 @@
+package tda593.hotel.california.integration;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CheckInIT {
+	
+	@Test
+	public void customerCanCheckInIT() {
+		fail("NYI");
+	}
+}
