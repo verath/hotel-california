96a1035ac850dab217f7a7de92c4c1a940c7cbbb
Hallin <antonhallin3@gmail.com>
2015-01-01 21:04:06 +0100

    Work in Register purchase test

--- integration-tests/test/tda593/hotel/california/integration/RegisterPurchaseTest.java
+++ integration-tests/test/tda593/hotel/california/integration/RegisterPurchaseTest.java
@@ -1,12 +1,12 @@
 package tda593.hotel.california.integration;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
+import org.eclipse.emf.common.util.BasicEList;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -26,6 +26,8 @@ import tda593.hotel.california.booking.LegalEntityManager;
 import tda593.hotel.california.booking.Organization;
 import tda593.hotel.california.booking.Person;
 import tda593.hotel.california.facilities.AdminRoomManager;
+import tda593.hotel.california.facilities.Room;
+import tda593.hotel.california.facilities.RoomManager;
 import tda593.hotel.california.facilities.RoomType;
 
 public class RegisterPurchaseTest extends AbstractHotelCaliforniaIntegrationTest {
@@ -38,11 +40,12 @@ public class RegisterPurchaseTest extends AbstractHotelCaliforniaIntegrationTest
 	private AdminServiceManager adminServiceManager;
 	private AdminDiscountManager discountManager;
 	private BookingManager bookingManager;
+	private RoomManager roomManager;
 	
 	private Calendar c = Calendar.getInstance();
 	
 	private Service bananas, champagne;
-	private Person customer1;
+	private Person customer;
 	private Organization customer2;
 	private String validCreditCardNumber = "2190 8910 1029 8908 6752";
 	private String validCCV = "902";
@@ -59,6 +62,7 @@ public class RegisterPurchaseTest extends AbstractHotelCaliforniaIntegrationTest
 		adminServiceManager = managersHandler.getAdminServiceManager();
 		discountManager = managersHandler.getAdminDiscountManager();
 		bookingManager = managersHandler.getBookingManager();
+		roomManager = managersHandler.getRoomManager();
 		
 		LegalEntityManager legalEntityManager = managersHandler.getLegalEntityManager();
 		AdminRoomManager adminRoomManager = managersHandler.getAdminRoomManager();
@@ -68,22 +72,28 @@ public class RegisterPurchaseTest extends AbstractHotelCaliforniaIntegrationTest
 		RoomType roomType = adminRoomManager.getRoomTypes().get(0);
 		
 		// Create a room
-		adminRoomManager.addGuestRoom(1+"1", 1, "N/A", null, null, roomType, 2, 0);
+		adminRoomManager.addGuestRoom("1", 1, "N/A", null, null, roomType, 2, 0);
 		
 		// Create a customer
-		customer1 = legalEntityManager.createPerson("Thomas", "Anderson", "1", "0712345678", "neo@matrix.com");	
+		customer = legalEntityManager.createPerson("Thomas", "Anderson", "1", "0712345678", "neo@matrix.com");	
 		
 		// Set up some services
 		bananas = adminServiceManager.createService("Bananas", 12.250);
 		champagne = adminServiceManager.createService("Champagne", 99.99);
 		
 		// Create a booking
-		c.set(2015, 5, 10);
+		c.set(2014, 5, 10);
 		Date from = c.getTime();
 		c.set(2015, 5, 14);
 		Date to = c.getTime();
 		
-		bookingManager.createBooking(from, to, legalEntityManager.getPerson("1"), roomType);
+		Room room = roomManager.getRooms().get(0);
+		Booking booking = bookingManager.createBooking(from, to, customer, room.getRoomType());
+		bookingManager.registerRoom(booking, room);
+		BasicEList<Person> guests = new BasicEList<Person>();
+		guests.add(customer);
+		bookingManager.checkIn(booking, guests);
+		billManager.createBookingBill(customer, booking);
 	}
 
 	/**
@@ -94,17 +104,31 @@ public class RegisterPurchaseTest extends AbstractHotelCaliforniaIntegrationTest
 		// System asks for a room number.
 		// Actor enters the room number.
 		// Assume: room number is valid and there is an active room stay.
-		Booking activeBooking = bookingManager.getActiveBooking("11");
+		Booking activeBooking = bookingManager.getActiveBooking("1");
 		// System returns the registered legal entities on the room stay (customer and all registered guests that are also customers to the hotel).
 		// Assume: the actor confirms the legal entity making the purchase.
 		// TODO: How do we know this legal entity has valid credit card information?
 		LegalEntity customer = activeBooking.getRoomStay().getRegisteredPersons().get(0);
 		
-		Bill bill = billManager.getBookingBill(activeBooking);
+		Bill bill = billManager.getBills(customer).get(0);
 		
-		if(bill.isPublished()) {
-			billManager.createBookingBill(customer, activeBooking);
-		}
+		billManager.billItem(bill, 1, bananas);
+		billManager.billItem(bill, 7, champagne);
+		
+		int quantity;
+		Service service;
+		
+		quantity = bill.getPurchases().get(0).getQuantity();
+		service = bill.getPurchases().get(0).getService();
+		
+		assertEquals(1, quantity);
+		assertEquals(bananas, service);
+		
+		quantity = bill.getPurchases().get(1).getQuantity();
+		service = bill.getPurchases().get(1).getService();
+		
+		assertEquals(7, quantity);
+		assertEquals(champagne, service);
 		
 	}
 	
